b7ab742b404994f3b76929d46ead74ffecfe8967
Yuri Stanislavsky <stanislavskiy@gmail.com>
2020-03-02 17:40:22 +0200

    Initial Commit

---
+++ GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Юрий Станиславский on 02.03.2020.
+//  Copyright © 2020 Yuri Stanislavsky. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
